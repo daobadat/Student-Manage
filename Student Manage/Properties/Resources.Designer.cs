7d9abba1563a4fdc3a8871238366ecc0d58045e6
Cow <lookingboy2033@gmail.com>
2024-03-02 03:42:33 +0700

    commit 3:41 usecontrol

--- Student Manage/Properties/Resources.Designer.cs
+++ Student Manage/Properties/Resources.Designer.cs
@@ -113,6 +113,16 @@ namespace Student_Manage.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap _65969021 {
+            get {
+                object obj = ResourceManager.GetObject("65969021", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap _6741122 {
             get {
                 object obj = ResourceManager.GetObject("6741122", resourceCulture);
@@ -146,6 +156,16 @@ namespace Student_Manage.Properties {
         internal static System.Drawing.Bitmap _9051433 {
             get {
                 object obj = ResourceManager.GetObject("9051433", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _90514331 {
+            get {
+                object obj = ResourceManager.GetObject("90514331", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
